a23d354d1431c411c1534de93840ab9b791695eb
vikas dakle <vikas@gmail.com>
2023-07-21 18:28:01 +0530

    initial commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://driving-zuz4.onrender.com",
     credentials: true
 }))
 
